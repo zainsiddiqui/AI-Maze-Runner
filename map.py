1a11f887a5789ce4378768e107026cf821e7259e
Zain <zainsid11@gmail.com>
2020-02-04 22:58:13 -0500

    adding probability p into generatemap function

--- map.py
+++ map.py
@@ -3,7 +3,8 @@ import random as r
 
 def main():
   inputDim = input("Enter dimension of map: ")
-  map=generateMap(int(inputDim))
+  inputP = input("Enter probability 0<p<1: ")
+  map=generateMap(int(inputDim),float(inputP))
   printMap(map)
 
 def printMap(a):
@@ -11,15 +12,16 @@ def printMap(a):
   print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in a]))
   print()
 
-def generateMap(dim):
+def generateMap(dim,p):
   map = [[0 for x in range(dim)] for y in range(dim)]
-  p = [0,1]
+  v = [0,1]
+  weights = [1-p,p]
   for row in range(len(map)):
     for col in range(len(map)):
-      map[row][col]= r.choice(p)
-
-  map[0][0] = "s"
-  map[dim-1][dim-1] = "x"
+      value = r.choices(v,weights)
+      map[row][col] = value[0]
+  map[0][0] = "S"
+  map[dim-1][dim-1] = "G"
   return map
 
 

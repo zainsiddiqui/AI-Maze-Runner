9a6f6bcee40bfc0d91a2eba9e8cbcb6f44f332c0
Zain <zainsid11@gmail.com>
2020-02-16 02:46:34 -0500

    fire

--- fire.py
+++ fire.py
@@ -2,6 +2,7 @@ import random as r
 import numpy as np
 import matplotlib.pyplot as plt
 import copy
+from searches import Coordinates
 from map import printMap
 
 def main():
@@ -9,9 +10,54 @@ def main():
     inputP = input("Enter probability for FIRE map 0<=p<=1: ")
     inputQ = input("Enter flammabilty for FIRE map 0<=y<=1: ")
     q = float(inputQ)
-    map = generateFireMaze(int(inputDim),float(inputP))
+    result = generateFireMaze(int(inputDim),float(inputP))
+    map = result[0]
+    firex = result[1]
+    firey = result[2]
     mazeVisual(map)
+    current = (0, 0)
+    originFire = Coordinates(firex, firey)
+    path = []
+    while (True):
+
+        score = 0
+        updateFire(map, q)
+        neighbors = get_neighbors(map,current)
+
+        for x in neighbors:
+            result = calculateHeaurstic(map, x, originFire)
+            if result[1] == True:
+                path.append((x,x))
+                print(path) 
+                return
+            if result[0] > score:
+                score = result[0]
+                current = x
+        #print(current)
+        (x,y) = current
+        map[x][y]=9
+        path.append(current)
+        
+            
+
     plt.show()
+
+def get_neighbors(map,current):
+
+    (row,column) = current
+    neighbors = []
+    
+    for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0)]:
+        position = (row + new_position[0], column + new_position[1])
+        if position[0] > (len(map) - 1) or position[0] < 0 or position[1] > (len(map[len(map)-1]) -1) or position[1] < 0:
+            continue
+        if map[position[0]][position[1]] != 0:
+            continue
+        neighbors.append(position)
+                          
+    return neighbors
+    
+  
 
 def num_burning_neighbors(temp, x, y):
     count = 0
@@ -35,9 +81,8 @@ def updateFire(map, q):
                 v = [2, 0]
                 weights = [prob, 1 - prob]
                 value = r.choices(v, weights)
-                temp[x][y] = value[0]
-    
-    return map
+                temp[x][y] = value[0]  
+    return temp
 
             
 
@@ -58,7 +103,7 @@ def generateFireMaze(dim, p):
         y = r.randrange(0, dim - 1)
     map[x][y] = 2
 
-    return map
+    return [map, x, y]
 
 def mazeVisual(map):
     temp = copy.deepcopy(map)
@@ -81,5 +126,15 @@ def mazeVisual(map):
     plt.title("MAZE")
     plt.xlabel('Column (MAZE)')
     plt.ylabel('Row (MAZE)')
+    
+## Calculates manhattan distance from current coordinate to origin coordinate
+def calculateHeaurstic(map, currentCell, origin_fire):
+    (x,y) = currentCell
+    if (x == len(map)-1 and y == len(map)-1):
+        return [0,True]
+    else:
+        score = (abs(x - origin_fire.x) + abs(y - origin_fire.y))
+        return [score, False]
 
 main()
+
